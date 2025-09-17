375d7c8186222b1b277c94796f74cb702d5fc90e
Nkateko Tibane <nkatekotibane101@gmail.com>
2025-09-17 23:24:02 +0200

    added error subroutines

--- src/memory.asm
+++ src/memory.asm
@@ -1,9 +1,16 @@
 section .data
 	dump_size dd 256
 	msg_stack db "-- STACK -- ", 0xa
+	stack_len equ $ - msg_stack
+
 	done_stack db "-- DONE STACK -- ", 0xa
 	dstack_len equ $ - done_stack
-	stack_len equ $ - msg_stack
+	
+	 err_attach   db "ptrace attach failed", 0x0A
+    	err_attach_len equ $ - err_attach
+    	err_getregs  db "ptrace getregs failed", 0x0A
+    	err_getregs_len equ $ - err_getregs
+
 	newline db 0xa
 	
 section .bss
@@ -20,8 +27,6 @@ section .bss
         ;         ds, es, fs, gs;
 	;};
 
-
-
 global Memory
 global ViewMemory
 global ModifyMemory
@@ -31,20 +36,19 @@ section .text
 Memory:
 	push rbp
 	mov rbp, rsp
-	
 	mov [pid], rdi
 
 .ptrace_attach:
 	; ptrace_attach = 16
 	mov rax, 101
 	mov rdi, 16
-	lea rsi, [pid]
+	mov rsi, [pid]
 	xor rdx, rdx
 	xor r10, r10
 	syscall
 
 	cmp rax, 0
-	jl .done
+	jl .err_attach
 	
 .ptrace_getregs:
 	mov rax, 101
@@ -55,7 +59,7 @@ Memory:
 	syscall
 
 	cmp rax, 0
-	jl .detach
+	jl .err_getregs
 
 	; print -- stack --
 	mov rax, 1
@@ -70,19 +74,22 @@ Memory:
 
 .stack_loop:
 	cmp rcx, [dump_size]
-	jge .done
+	jge .detach
 
 .ptrace_peekdata_1:
 	; dump stack
 	mov rax, 101
 	mov rdi, 2 ; PTRACE_PEEKDATA
 	mov rsi, [pid]
-	lea rdx, [r8+rcx] ; [regs + 19*8] ; rsp
+	;lea rdx, [r8+rcx] ; [regs + 19*8] ; rsp
+	mov rdx, r8
+	add rdx, rcx
 	xor r10, r10
 	syscall
 	
 	cmp rax, 0
 	jl .detach
+	
 	; store first 8 bytes
 	mov [dumped], rax
 
@@ -90,7 +97,9 @@ Memory:
 	mov rax, 101
 	mov rdi, 2
 	mov rsi, [pid]
-	lea rdx, [r8+rcx]
+	;lea rdx, [r8+rcx]
+	mov rdx, r8
+	add rdx, rcx
 	add rdx, 8
 	xor r10, r10
 	syscall
@@ -109,6 +118,7 @@ Memory:
 	add rcx, 16
 	jmp .stack_loop
 	
+
 .detach:
 	mov rax, 101
 	mov rdi, 17
@@ -117,7 +127,7 @@ Memory:
 	xor r10, r10
 	syscall
 
-.done:	
+.done_stack:	
 	; print -- done stack --
 	mov rax, 1
 	mov rdi, 1
@@ -125,7 +135,31 @@ Memory:
 	mov rdx, dstack_len
 	syscall
 	
+.done:
 	mov rax, 0
 	mov rsp, rbp
 	pop rbp
 	ret
+
+.err_attach:
+    ; write error and return -1
+    mov rax, 1
+    mov rdi, 2
+    mov rsi, err_attach
+    mov rdx, err_attach_len
+    syscall
+    mov rax, -1
+    mov rsp, rbp
+    pop rbp
+    ret
+
+.err_getregs:
+    mov rax, 1
+    mov rdi, 2
+    mov rsi, err_getregs
+    mov rdx, err_getregs_len
+    syscall
+    mov rax, -1
+    mov rsp, rbp
+    pop rbp
+    ret
